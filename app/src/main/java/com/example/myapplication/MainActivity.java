6c87dabf9e4ad6addc138ef6a71783b8b79be611
Robert <robertapikyan@mail.ru>
2019-07-06 05:20:28 -0700

    First commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,11 @@ public class MainActivity extends AppCompatActivity{
 
         toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     @Override
